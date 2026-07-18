25abb46176e3ba3b9840553afb0dc53e842081a8
sujalsubedi06 <sujal.subedi96@gmail.com>
2026-07-19 01:49:17 +0545

    Added the assets of SajiloPass

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,3 +1,4 @@
+import { px } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -13,10 +14,10 @@ export default function Logo({
   return (
     <Link
       href="#home"
-      className="flex items-center gap-3"
+      className="flex items-center gap-2 transition-transform duration-300 hover:scale-[1.02]"
     >
       <Image
-        src="/logos/logo-mark.png"
+        src="/logos/logo_primary.png"
         alt="SajiloPass Logo"
         width={size}
         height={size}
